2d87d17363f56abd354a9c5f5d7eca9bc2207262
Stephanie Landgraf <stlandgraf1234@gmail.com>
2020-04-29 22:23:01 -0500

    first attempt at the randomizer block

---
+++ index.js
@@ -0,0 +1,19 @@
+// randomizing code block
+
+
+// the final password will be generated from a new array, based on the chosen criteria
+
+
+let endWordBank = concat_arrays;
+
+function generatePassword () 
+{
+    var final_word = "";
+    var endArray = concat_arrays
+    for(var i=0; i < length; i++)
+    {
+    final_word <= concat_arrays.charAt(Math.floor((Math.random() * concat_arrays.length))
+    }
+return final_word;
+}
+    console.log(generatePassword(lengthNum));
