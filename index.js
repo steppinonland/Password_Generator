206062045dd422a4d6fc43fd50ba924afa308a6c
Stephanie Landgraf <stlandgraf1234@gmail.com>
2020-05-01 15:03:13 -0500

    wrote out the basic framework of what I'm trying to do

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// naming all the arrays and variables:
+
 
 // first step: click button > confirm box 1 > confirm box ; ELSE close window
 
@@ -7,20 +7,60 @@ var upperArray = confirm("Do you want it to contain uppercase letters?");
 var lowerArray = confirm("Do you want it to contain lowercase letters?");
 var numAarray = confirm("Do you want it to include numbers?");
 var specialArray = confirm("Do you want it to include special characters?");
+var endArray = [];
+var wordLength = prompt("How long would you like the password to be? Enter in a number between 8 and 120.")
 
+if (goGenerate){
+    alert("You can choose whether your password will include uppercase letters, lowercase letters, numbers, and special characters.")
+}
 
-goGenerate() = true 
-    var UpperArray = confirm("Do you want it to include uppercase letters")
+if (upperArray){
+    let endArray = upperArray.map((element) => {
+        return upperArray;
+    }
 
+if (lowerArray){
+    let endArray = lowerArray.map((element) => {
+        return lowerArray;
+        }
+
+if (numArray){
+    let endArray = numArray.map((element) => {
+        return numArray;
+    }
+}
+
+if (specialArray){
+    let endArray = specialArray.map((element) => {
+        return specialArray;
+    }
+}
+// the entry for word length must be between 8 and 120 characters
+let wordLength = {
+    " " >= 8 && <= 120;
+}
+    if wordLength ! >=8 || <= 120 {
+        alert("Invalid Number. Please try again.")
+    }
+// if it's a valid number, that number becomes the length of the password generated
+    else if {
+        wordLength = endArray.length;
+    }
+    
 // randomizing code block
+
+let endArray = [" "]
+
+function generatePassword {
+    Math.floor(Math.random(endArray.length));
+}
 
 
 // the final password will be generated from a new array, based on the chosen criteria
 
+var finalWord = alert("Your password is:")
 
-let endWordBank = concat_arrays;
-
-function generatePassword () 
+}
 {
     var final_word = "";
     var endArray = concat_arrays
