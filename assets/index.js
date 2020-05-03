4c4ffe98b144145e7b7a26dad561703b503cfab8
Stephanie Landgraf <stlandgraf1234@gmail.com>
2020-05-02 21:28:58 -0500

    fixed conditional. fixed the spelling of array in specialArray name

--- assets/index.js
+++ assets/index.js
@@ -5,7 +5,6 @@
 
 //  naming all the arrays and variables:
 /*
-            const specialArray = [" ", "!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\", "^", "`", "{", "|", "}", "~"];
             *
 
 /*
@@ -19,18 +18,24 @@ function generatePassword () {
     const upperArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
     const lowerArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
     const numArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-    var goGenerate = confirm("Do you want to generate a password?");
+    const specialArray = ["#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "["];
+
     var allowUpperArray = confirm("Do you want it to contain uppercase letters?");
+
     var allowLowerArray = confirm("Do you want it to contain lowercase letters?");
-    var allowNumAarray = confirm("Do you want it to include numbers?");
+
+    var allowNumArray = confirm("Do you want it to include numbers?");
+
+    var allowSpecialArray = confirm("Do you want it to include special characters?");
+
+    var endArray = [];
 
     var wordLength = (parseInt(prompt("How long would you like the password to be? Enter in a number between 8 and 120.")) - 1);
     if (wordLength <= 8 || wordLength >= 120)
     {
         alert("Try a different number. Must enter a number between 8 and 120.");
+        return wordLength = (parseInt(prompt("How long would you like the password to be? Enter in a number between 8 and 120.")) - 1);
     }
-    
-    var endArray = [];
     if (allowUpperArray) {
         upperArray.forEach((letter) => {
             endArray.push(letter);
@@ -39,6 +44,16 @@ function generatePassword () {
     if (allowLowerArray) {
         lowerArray.forEach((letter) => {
             endArray.push(letter);
+        })
+    }
+    if (allowNumArray) {
+        numArray.forEach((number) => {
+            endArray.push(number);
+        })
+    }
+    if (allowSpecialArray) {
+        specialArray.forEach((character) => {
+            endArray.push(character);
         })
     }
 
@@ -56,7 +71,6 @@ function generatePassword () {
     document.getElementById("passwordDisplay").value=finalWord;
 }
     /*
-    var allowSpecialArray = confirm("Do you want it to include special characters?");*/
     
 
  
